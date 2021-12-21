31f2b0a10e21351dfeebd0b36a46d21073e0177e
Julien <julien@ripouteau.com>
2021-12-21 17:49:56 +0100

    ✨ added package typings

---
+++ adonis-typings/grpc-consumer.ts
@@ -0,0 +1,25 @@
+declare module '@ioc:Adonis/Addons/GrpcConsumer' {
+  import { Options } from '@grpc/proto-loader'
+  import { Client } from '@grpc/grpc-js'
+
+  export interface GrpcConsumerBaseContract {
+    getClient<T extends Client>(name: string): T | undefined
+    closeAll(): Promise<void>
+  }
+
+  export interface GrpcClientConfig {
+    name: string
+    options: {
+      package: string
+      protoPath: string
+      serviceName: string
+      url: string
+      packageDefinitionOptions?: Options
+    }
+  }
+
+  export interface GrpcConsumerConfig {
+    clients: GrpcClientConfig[]
+    verbose: boolean
+  }
+}
---
+++ adonis-typings/index.ts
@@ -0,0 +1,1 @@
+/// <reference path="./grpc-consumer.ts" />
